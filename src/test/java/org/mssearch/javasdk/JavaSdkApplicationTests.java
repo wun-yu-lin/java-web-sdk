8c6e6ba0aa82df5e1b8f4d75095a0c6a7ec80226
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2025-02-28 10:23:13 +0800

    first version

---
+++ src/test/java/org/mssearch/javasdk/JavaSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mssearch.javasdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
