388fc51b0d831bd5f861159cba4b13e8ba0db37c
wun-yu-lin <t928394558@gmail.com>
2025-05-14 16:27:35 +0800

    ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD

--- src/main/java/sdk/mssearch/javasdk/ApplicationContextHolder.java
+++ src/main/java/sdk/mssearch/javasdk/ApplicationContextHolder.java
@@ -1,5 +1,6 @@
 package sdk.mssearch.javasdk;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Order(Integer.MIN_VALUE)
+@SuppressFBWarnings("ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD")
 public class ApplicationContextHolder implements ApplicationContextAware {
 
     private static ApplicationContext context;
