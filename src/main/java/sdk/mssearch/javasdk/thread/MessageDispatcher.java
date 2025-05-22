bc5ee11a7349a89fbec77d63f957a83d25c8eb1b
wun-yu-lin <t928394558@gmail.com>
2025-05-23 01:30:10 +0800

    mail to async

---
+++ src/main/java/sdk/mssearch/javasdk/thread/MessageDispatcher.java
@@ -0,0 +1,50 @@
+package sdk.mssearch.javasdk.thread;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+import sdk.mssearch.javasdk.logger.SdkLoggerFactory;
+
+import java.util.concurrent.*;
+
+@Component
+public class MessageDispatcher {
+
+    private final Logger logger = SdkLoggerFactory.getLogger(MessageDispatcher.class);
+    private ThreadPoolExecutor executor;
+    private final static int MAX_QUEUE_SIZE = 1024;
+    private final static int DEFAULT_THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors() / 2;
+    private final static int DEFAULT_THREAD_POOL_MAX = Runtime.getRuntime().availableProcessors();
+    private final static TimeUnit DEFAULT_TIME_UNIT = TimeUnit.SECONDS;
+    private final static int DEFAULT_KEEP_ALIVE_TIME = 60;
+    private final static String THREAD_POOL_NAME = "sdk-message-dispatcher-worker";
+
+    @PostConstruct
+    void init() {
+        this.executor = new ThreadPoolExecutor(DEFAULT_THREAD_POOL_SIZE,
+                DEFAULT_THREAD_POOL_MAX,
+                DEFAULT_KEEP_ALIVE_TIME,
+                DEFAULT_TIME_UNIT,
+                new ArrayBlockingQueue<>(MAX_QUEUE_SIZE),
+                new NamedThreadFactory(THREAD_POOL_NAME)
+        );
+    }
+
+    public void publish(Runnable runnable) {
+        logger.info("Publishing {}", runnable);
+        try {
+            executor.execute(runnable);
+        } catch (Exception e) {
+            logger.warn("Failed to publish {}, queue size: {} ", runnable, executor.getQueue().size(), e);
+        }
+
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        executor.shutdown();
+    }
+
+}
---
+++ src/main/java/sdk/mssearch/javasdk/thread/NamedThreadFactory.java
@@ -0,0 +1,20 @@
+package sdk.mssearch.javasdk.thread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+    private final String prefix;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+    public NamedThreadFactory(String prefix) {
+        this.prefix = prefix;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setName(prefix + "-" + threadNumber.getAndIncrement());
+        return t;
+    }
+}
